e469d0c82b45aa1f785515b8be7281b8fbf7b726
zain Imtiaz <zenimtiazz@gmail.com>
2022-01-21 15:24:54 +0100

    working on posts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -23,7 +24,6 @@ Route::post('/register',[RegisterController::class,'store']);
 Route::get('/login',[LoginController::class,'index'])->name('login');
 Route::post('/login',[LoginController::class,'store']);
 
-Route::get('/posts', function () {
+Route::get('/posts',[PostController::class,'index'])->name('posts');
+Route::post('/posts',[PostController::class,'store']);
 
-    return view('posts.index');
-});

dc8f1b439d2cc27e3f288b913c46aa7e98f086a5
zain Imtiaz <zenimtiazz@gmail.com>
2022-01-25 14:26:06 +0100

    laravel tincker and foreign key

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
 public function index(){
-    $posts =Post::paginate(2);
+    $posts =Post::paginate(20);
     return view('posts.index',[
         'posts' => $posts
     ]);
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,9 +28,20 @@
               <a href="" class="font-bold">{{ $post->user->name }}</a>
               <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
               <p class="mb-2">{{ $post->body }}</p>
+              <div class="flex items-center">
+                  <form action="" method="post" class="mr-1">
+                     @csrf
+                    <button type="submit" class="text-blue-500" >Like</button>
+                  </form>
+                  <form action="" method="post" class="mr-1">
+                    @csrf
+                    <button type="submit" class="text-blue-500">DisLike</button>
+                </form>
+
                </div>
+               
            @endforeach
-           {{ $posts->links() }}
+          {{ $posts->links() }}
           @else
          <p>There are no posts</p>
           @endif

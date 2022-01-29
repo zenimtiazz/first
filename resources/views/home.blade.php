ea5d4102a91cea3c3fd5b86fdfacdd4e57ab4b36
zain Imtiaz <zenimtiazz@gmail.com>
2022-01-29 12:30:46 +0100

    doneeee

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 
 <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
   <div
-    class="relative z-30 p-5 text-3xl text-white bg-black-300 bg-opacity-50 rounded-xl">
+    class="relative z-30 p-5 text-6xl text-white bg-blue-300 bg-opacity-50 rounded-xl">
     Welcome to my website!
   </div>
  
@@ -15,7 +15,7 @@
     class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
   >
     <source
-      src="https://vod-progressive.akamaized.net/exp=1643411610~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3256%2F5%2F141283020%2F423064707.mp4~hmac=237eab56a1b30573c9e776507c3aba461418285995899400b7c2f463bc49f45d/vimeo-prod-skyfire-std-us/01/3256/5/141283020/423064707.mp4?filename=Futuristic+-+896.mp4"
+      src="https://vod-progressive.akamaized.net/exp=1643470095~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F3267%2F6%2F166339073%2F530016613.mp4~hmac=57870c7d1c93da2474cd55ee805130d2d01512cb564b213fbd7d932a660e666d/vimeo-prod-skyfire-std-us/01/3267/6/166339073/530016613.mp4?filename=Computer+Keyboard+-+3188.mp4"
       type="video/mp4"
     />
     Your browser does not support the video tag.
